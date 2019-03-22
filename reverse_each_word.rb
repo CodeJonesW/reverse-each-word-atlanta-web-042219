16951a23a24bdb250a1e0932f4b19e2d722e6e42
Will Jones <willjones2010@gmail.com>
2019-03-22 17:32:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,19 +1,9 @@
-# def reverse_each_word(string)
-# new = ""
-# reversed_array = []
-# new_array = string.split(" ")
-# new_array.each do |word|
-# reversed_array.push(word.reverse)
-# new = reversed_array.join(" ") 
-# end
-# return new
-# end
 def reverse_each_word(string)
 new = ""
 reversed_array = []
 new_array = string.split(" ")
 new_array.collect do |word|
- reversed_array.push(word.reverse)
- new = reversed_array.join(" ")
+ new << word.reverse + " "
 end
+return new
 end
